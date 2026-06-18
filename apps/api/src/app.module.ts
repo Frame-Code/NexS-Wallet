d9c10ff54ac0a763ccb96cecccd846a790d45172
Ferxho <calderonf059@gmail.com>
2026-06-18 13:08:02 -0500

    feat: add ThrottlerGuard global to app.module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,6 +1,7 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { ThrottlerModule } from '@nestjs/throttler';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 import { FirebaseModule } from './firebase/firebase.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { WebhookMiddleware } from './common/middlewares/webhook.middleware';
@@ -12,13 +13,19 @@ import { WebhookMiddleware } from './common/middlewares/webhook.middleware';
     AuthModule,
     ThrottlerModule.forRoot([{ ttl: 60_000, limit: 30 }]),
   ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
-export class AppModule implements NestModule{
+export class AppModule implements NestModule {
 
   configure(consumer: MiddlewareConsumer) {
     consumer
-    .apply(WebhookMiddleware)
-    .forRoutes('webhooks/helius', 'webhooks/alchemy');
+      .apply(WebhookMiddleware)
+      .forRoutes('webhooks/helius', 'webhooks/alchemy');
   }
-  
+
 }
